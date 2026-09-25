ae7e74679df15f0f9eedd24268faf087f8cdfff9
molismwaura3-dev <molismwaura3@gmail.com>
2026-09-25 03:36:27 +0300

    Update from Deriv App Builder

--- packages/core/src/react/index.ts
+++
@@ -1,5 +0,0 @@
-export { useDerivWS } from './useDerivWS';
-export { useActiveSymbols } from './useActiveSymbols';
-export { useTicks, FORGET_ACK_TIMEOUT_MS } from './useTicks';
-export { useProposal } from './useProposal';
-export { useBuy } from './useBuy';
